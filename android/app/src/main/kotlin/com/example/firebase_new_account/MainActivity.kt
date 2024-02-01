93b7ad989abedc6cb4026975917fa2f8edab89cb
Samara1404 <stambekova1404@gmail.com>
2024-02-01 23:51:51 +0300

    firebase_new_account

---
+++ android/app/src/main/kotlin/com/example/firebase_new_account/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebase_new_account
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app.dart
@@ -0,0 +1,40 @@
+
+import 'package:firebase_new_account/firebase_service.dart';
+import 'package:firebase_new_account/screens/user_info_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/auth_screen.dart';
+
+final GlobalKey<NavigatorState> kNavigatorKey = GlobalKey<NavigatorState>();
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseService().onListenUser((user) {
+      if (user == null) {
+        Navigator.push(kNavigatorKey.currentContext!, MaterialPageRoute(
+        builder: (_) => AuthScreen()));
+      } else {Navigator.push(kNavigatorKey.currentContext!, MaterialPageRoute(
+        builder: (_) => UserInfoScreen(user: user)));}
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: kNavigatorKey,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+              ),
+      home: AuthScreen(),
+    );
+  }
+}
